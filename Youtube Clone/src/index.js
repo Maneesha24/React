cc297b873ce2779325159850efda16fe19ab593e
Maneesha <32819661+Maneesha24@users.noreply.github.com>
2018-03-20 01:56:17 -1100

    Add files via upload

---
+++ Youtube Clone/src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import _ from 'lodash';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list'
+import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail'
+
+const API_KEY = 'AIzaSyCpAi9FIwCzRm2UsFaLWWx5tOD8QwFz8I8';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      videos : [],
+      selectedVideo : null
+    }
+    this.VideoSearch('Dogs');
+
+  }
+
+
+  VideoSearch(term){
+    YTSearch({key : API_KEY, term : term}, (videos) => {
+        this.setState({
+          videos : videos,
+        selectedVideo:videos [0]
+      });
+      });
+
+  }
+
+  render(){
+    const videoSearch = _.debounce((term) => {this.VideoSearch(term)},300);
+
+  return(
+    <div>
+    <SearchBar onSearchTermChange = {videoSearch}/>
+    <VideoDetail video = {this.state.selectedVideo} />
+    <VideoList onVideoSelect = {selectedVideo => this.setState({selectedVideo})} videos = {this.state.videos}/>
+    </div>
+  )};
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
